2bdc3deb093e44bc16bc170b4709f2e0dcac7860
aegan123 <juhani@fmail.co.uk>
2018-07-10 17:26:19 +0300

    updated

--- riskiwww/settings.py
+++ riskiwww/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 # Application definition
 
 INSTALLED_APPS = [
+	'eventsignup.apps.EventsignupConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
