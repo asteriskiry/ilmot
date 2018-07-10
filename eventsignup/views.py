c5a75dc18ec4099570e8f2a8f45cf11aea4a9d5c
aegan123 <juhani@fmail.co.uk>
2018-07-10 17:25:52 +0300

    created view stubs

--- eventsignup/views.py
+++ eventsignup/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from .models import TapahtumaTyypit, TapahtumanOmistaja, Tapahtumat, Sitsit, Vuosijuhla, Ekskursio
+from .models import MuuTapahtuma, Osallistuja, Arkisto
+
+def index(request):
+	return HttpResponse("Welcome!")
+
+def stats(request, uid):
+	pass
+
+def signup(request, uid):
+	pass
+
+def archive(request, uid):
+	pass
+
+def new(request, **kwargs):
+	pass
