33546f654b5c7cd9719b1c4f0c0b39ca8ad90bd2
Juhani <juhani@fmail.co.uk>
2018-07-11 16:05:21 +0200

    Added floppyforms

--- riskiwww/settings.py
+++ riskiwww/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 # Application definition
 
 INSTALLED_APPS = [
+	'floppyforms',
 	'eventsignup.apps.EventsignupConfig',
     'django.contrib.admin',
     'django.contrib.auth',
