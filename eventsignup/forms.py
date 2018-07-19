b7ff5d6f390f245bda07fcada250eb21eb8be9a3
Juhani <juhani@fmail.co.uk>
2018-07-19 16:36:24 +0200

    html5 style date input working. datetime not

--- eventsignup/forms.py
+++ eventsignup/forms.py
@@ -1,28 +1,58 @@
 from django.forms import ModelForm
 from django import forms
 from .models import Sitz, Annualfest, OtherEvent, Excursion, Participant
+from .widgets import *
 
 class SitzForm(ModelForm):
+	def __init__(self, *args, **kwargs):
+		super(SitzForm, self).__init__(*args, **kwargs)
+		self.fields['date'].widget = MyDateInput(attrs={'class':'date'})
+		self.fields['signup_starts'].widget = MyDateTimeInput(attrs={'class':'date'})
+		self.fields['signup_ends'].widget = MyDateTimeInput(attrs={'class':'date'})
 	class Meta:
 		model = Sitz
 		fields = '__all__'
 
 class AnnualfestForm(ModelForm):
+	def __init__(self, *args, **kwargs):
+		super(AnnualfestForm, self).__init__(*args, **kwargs)
+		self.fields['date'].widget = MyDateInput(attrs={'class':'date'})
+		self.fields['signup_starts'].widget = MyDateTimeInput(attrs={'class':'date'})
+		self.fields['signup_ends'].widget = MyDateTimeInput(attrs={'class':'date'})
 	class Meta:
 		model=Annualfest
 		fields='__all__'
 
 class OtherEventForm(ModelForm):
+	def __init__(self, *args, **kwargs):
+		super(OtherEventForm, self).__init__(*args, **kwargs)
+		self.fields['date'].widget = MyDateInput(attrs={'class':'date'})
+		self.fields['signup_starts'].widget = MyDateTimeInput(attrs={'class':'date'})
+		self.fields['signup_ends'].widget = MyDateTimeInput(attrs={'class':'date'})
 	class Meta:
 		model=OtherEvent
 		fields='__all__'
 
 class ExcursionForm(ModelForm):
+	def __init__(self, *args, **kwargs):
+		super(ExcursionForm, self).__init__(*args, **kwargs)
+		self.fields['date'].widget = MyDateInput(attrs={'class':'date'})
+		self.fields['end_date'].widget = MyDateInput(attrs={'class':'date'})
+		self.fields['signup_starts'].widget = MyDateTimeInput(attrs={'class':'date'})
+		self.fields['signup_ends'].widget = MyDateTimeInput(attrs={'class':'date'})
+#		self.fields['passenger_count'].widget = MyNumberInput(attrs={'min':0})
+#		self.fields['email'].widget = MyEmailInput()
+
 	class Meta:
 		model=Excursion
 		fields='__all__'
 
 class CustomForm(ModelForm):
+	def __init__(self, *args, **kwargs):
+		super(CustomForm, self).__init__(*args, **kwargs)
+		self.fields['date'].widget = MyDateInput(attrs={'class':'date'})
+		self.fields['signup_starts'].widget = MyDateTimeInput(attrs={'class':'date'})
+		self.fields['signup_ends'].widget = MyDateTimeInput(attrs={'class':'date'})
 	#tähän jotain kenttiä vielä!!!
 	class Meta:
 		model=OtherEvent
@@ -38,4 +68,4 @@ class SitzSignupForm(ModelForm):
 		fields=['name','email','lihaton','holiton','avec','plaseeraus']
 
 class SelectTypeForm(forms.Form):
-	choice=forms.ChoiceField(label='Tapahtuman tyyppi',help_text='Valmiiksi määritellyillä tyypeillä tulee kyseiseen tapahtumaan soveltuva lomake. "Mukautettu" valinnalla voit mukauttaa tiedot haluamallasi tavalla.',choices=(('sitsit','Sitsit'),('vujut','Vuosijuhlat'),('eksku','Ekskursio'),('muu','Muu tapahtuma'),('custom','Mukautettu')))
+	choice=forms.ChoiceField(label='Tapahtuman tyyppi',help_text='Valmiiksi määritellyillä tyypeillä tulee kyseiseen tapahtumaan soveltuva lomake. "Mukautettu" valinnalla voit mukauttaa tiedot haluamallasi tavalla.',choices=(('sitsit','Sitsit'),('vuosijuhlat','Vuosijuhlat'),('ekskursio','Ekskursio'),('muutapahtuma','Muu tapahtuma'),('custom','Mukautettu')))
