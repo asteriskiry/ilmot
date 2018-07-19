26d5b6b2e0ca2c19a570e38ca5941b3f9937170e
Juhani <juhani@fmail.co.uk>
2018-07-19 12:08:57 +0200

    updated

--- omat/helpers.py
+++ omat/helpers.py
@@ -1,6 +1,7 @@
 # Erilaiset omat apufunktiot tänne.
 # Käyttö: from omat import helpers ja helpers.<funktion_nimi>
 from eventsignup.models import Events, Sitz, Annualfest, Excursion, OtherEvent
+from eventsignup.forms import AnnualfestForm, ExcursionForm, OtherEventForm, SitzForm
 import random
 from django.core.exceptions import ObjectDoesNotExist
 
@@ -13,36 +14,37 @@ def getUid():
 	return uid
 
 def getForm(event_type,request):
-#	form=None
-	if(event_type=='Sitsit'):
-#		form=SitzForm(request.POST)
-		return SitzForm(request.POST)
-	elif(event_type=='Vuosijuhlat'):
-#		form=AnnualfestForm(request.POST)
-		return AnnualfestForm(request.POST)
-	elif(event_type=='Ekskursio'):
-#		form=ExcursionForm(request.POST)
-		return ExcursionForm(request.POST)
-	elif(event_type=='Muu'):
-#		form=OtherEventForm(request.POST)
-		return OtherEventForm(request.POST)
-#	return form
+	form=None
+	print(event_type)
+	if(event_type=='sitsit'):
+		form=SitzForm(request.POST)
+#		return SitzForm(request.POST)
+	elif(event_type=='vuosijuhlat'):
+		form=AnnualfestForm(request.POST)
+#		return AnnualfestForm(request.POST)
+	elif(event_type=='ekskursio'):
+		form=ExcursionForm(request.POST)
+#		return ExcursionForm(request.POST)
+	elif(event_type=='muu'):
+		form=OtherEventForm(request.POST)
+#		return OtherEventForm(request.POST)
+	return form
 
 def getEvent(uid):
-	event=None
-	tempevent=None
-	events=Events.all()
-	for field in events:
-		try:
-			if(field['uid']==uid):
-				tempevent=Events.objects.get(uid=uid)
-		except ObjectDoesNotExist:
-			pass
-	if(tempevent['event_type']=='Sitsit'):
+#	event=None
+	tempevent=Events.objects.get(uid=uid)
+#	events=Events.objects.all()
+#	for field in events:
+#		try:
+#			if(field['uid']==uid):
+#				tempevent=Events.objects.get(uid=uid)
+#		except ObjectDoesNotExist:
+#			pass
+	if(tempevent['event_type']=='sitsit'):
 		return Sitz.objects.get(uid=uid)
-	elif(tempevent['event_type']=='Vuosijuhlat'):
+	elif(tempevent['event_type']=='vuosijuhlat'):
 		return Annualfest.objects.get(uid=uid)
-	elif(tempevent['event_type']=='Ekskursio'):
+	elif(tempevent['event_type']=='ekskursio'):
 		return Excursion.objects.get(uid=uid)
-	elif(tempevent['event_type']=='Muu'):
+	elif(tempevent['event_type']=='muu'):
 		return OtherEvent.objects.get(uid=uid)
