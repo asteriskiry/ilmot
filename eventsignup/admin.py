d0a7110792e512d148a735415e7195b315f34970
Juhani <juhani@fmail.co.uk>
2018-07-20 14:09:52 +0200

    added models to admin interface

--- eventsignup/admin.py
+++ eventsignup/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Sitz, Annualfest, OtherEvent, Excursion, Participant, EventType, EventOwner, Events, Archive
+
+admin.site.register(Sitz)
+admin.site.register(Annualfest)
+admin.site.register(OtherEvent)
+admin.site.register(Excursion)
+admin.site.register(Participant)
+admin.site.register(EventType)
+admin.site.register(EventOwner)
+admin.site.register(Events)
+admin.site.register(Archive)
