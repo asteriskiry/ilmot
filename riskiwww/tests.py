53c701e53241cea716202a0bf66aaa9b7f947dbf
rytilahti-juuso <rytilahti.juuso@gmail.com>
2018-08-08 16:04:51 +0300

    Korjattu rikki olevat contenttien sijainnit

--- riskiwww/tests.py
+++ riskiwww/tests.py
@@ -5,11 +5,16 @@ from django.test import TestCase
 from .views import home
 
 class HomeTests(TestCase):
-    def test_home_view_status_code(self):
-        url = reverse('home')
+    def test_event_add_view_status_code(self):
+        url = 'http://127.0.0.1:8000/eventsignup/event/add/'
         response = self.client.get(url)
         self.assertEquals(response.status_code, 200)
 
     def test_home_url_resolves_home_view(self):
         view = resolve('/')
         self.assertEquals(view.func, home)
+
+    def test_add_sitsit_view_status_code(self):
+        url = 'http://127.0.0.1:8000/eventsignup/event/add/sitsit/'
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
