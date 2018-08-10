5840a28e32b811c7423ebbea8d1d73d205e6236f
rytilahti-juuso <rytilahti.juuso@gmail.com>
2018-08-10 18:02:50 +0300

    lisätty ekskursioformin testit

--- eventsignup/tests.py
+++ eventsignup/tests.py
@@ -4,7 +4,50 @@ from django.urls import resolve
 from django.test import TestCase
 from django.contrib.auth.models import User
 from .models import EventType, EventOwner, Events, Participant, Sitz, Annualfest, Excursion, OtherEvent
-from .forms import SitzForm, AnnualfestForm
+from .forms import SitzForm, AnnualfestForm, ExcursionForm
+
+class EventsSignupExcursionTests(TestCase):
+    def setUp(self):
+        #EventType.objects.create(event_type='sitsit')
+        User.objects.create_user(username='test', email='lol@example.com', password='123')
+        EventType.objects.create(event_type='ekskursio')
+        EventOwner.objects.create(name='test')
+        self.client.login(username='test', password='123')
+
+    def test_contains_form(self):
+        url = 'http://127.0.0.1:8000/eventsignup/event/add/ekskursio'
+        response = self.client.get(url)
+        form = response.context.get('form')
+        self.assertIsInstance(form, ExcursionForm)
+
+    def test_csrf(self):
+        url = 'http://127.0.0.1:8000/eventsignup/event/add/ekskursio'
+        response = self.client.get(url)
+        self.assertContains(response, 'csrfmiddlewaretoken')
+
+    def test_new_sitz_form_send(self):
+        url = 'http://127.0.0.1:8000/eventsignup/event/add/ekskursio'
+        data = {
+        'name':'lol',
+        'place':'qtalo',
+        'date':'2018-01-01',
+        'end_date':'2018-01-02',
+        'start_time':'00.00.00',
+        'description':'kuvaus',
+        'signup_starts':'2018-02-02',
+        'signup_ends':'2018-03-03'
+        }
+        response = self.client.post(url, data)
+        self.assertTrue(EventType.objects.exists())
+
+    def test_sitz_form_invalid_form_data(self):
+        #Invalid post data should not redirect
+        #The expected behavior is to show the form again with validation errors
+        url = 'http://127.0.0.1:8000/eventsignup/event/add/vuosijuhlat'
+        response = self.client.post(url, {})
+        form = response.context.get('form')
+        self.assertEquals(response.status_code, 200)
+        self.assertTrue(form.errors)
 
 class EventsSignupAnnualfestTests(TestCase):
     def setUp(self):
@@ -25,7 +68,7 @@ class EventsSignupAnnualfestTests(TestCase):
         response = self.client.get(url)
         self.assertContains(response, 'csrfmiddlewaretoken')
 
-    def test_new_sitz_form_send(self):
+    def test_new_annualfest_form_send(self):
         url = 'http://127.0.0.1:8000/eventsignup/event/add/vuosijuhlat'
         data = {
         'name':'lol',
@@ -39,7 +82,7 @@ class EventsSignupAnnualfestTests(TestCase):
         response = self.client.post(url, data)
         self.assertTrue(EventType.objects.exists())
 
-    def test_sitz_form_invalid_form_data(self):
+    def test_annualfest_form_invalid_form_data(self):
         #Invalid post data should not redirect
         #The expected behavior is to show the form again with validation errors
         url = 'http://127.0.0.1:8000/eventsignup/event/add/vuosijuhlat'
