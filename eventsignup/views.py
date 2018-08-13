0bd4da2c84b6e7247e8cd2d8a9e9e8c489882825
Juhani <juhani@fmail.co.uk>
2018-08-13 16:09:30 +0200

    Validated form data is now used for helpers.getMiscInfo

--- eventsignup/views.py
+++ eventsignup/views.py
@@ -34,7 +34,7 @@ def signup(request, uid):
 		if(form.is_valid()):
 			#käsittele lomake
 			data=form.save(commit=False)
-			data.miscInfo=helpers.getMiscInfo(request.POST)
+			data.miscInfo=helpers.getMiscInfo(form.cleaned_data)
 			data.event_type=temp
 			data.save()
 			return HttpResponseRedirect('/eventsignup/thanks')
