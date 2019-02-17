08d60cc8b0fe344548db6b05ef379a91af057fe0
aegan123 <juhani@fmail.co.uk>
2019-02-17 16:06:37 +0200

    general code formatting to better conform to PEP8

--- eventsignup/models.py
+++ eventsignup/models.py
@@ -4,126 +4,138 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+
 # Kaikki uniikit tapahtumatyypit (esim. sitsit).
 class EventType(models.Model):
-	event_type=models.CharField(max_length=500,unique=True, verbose_name='Tapahtuman tyyppi')
-	def __str__(self):
-		return "Tapahtuman tyyppi: "+str(self.event_type)
+    event_type = models.CharField(max_length=500,unique=True, verbose_name='Tapahtuman tyyppi')
+
+    def __str__(self):
+        return "Tapahtuman tyyppi: "+str(self.event_type)
+
 
 # Tapahtuman järjestäjä (esim. Asteriski).
 class EventOwner(models.Model):
-	name=models.CharField(max_length=500, unique=True, verbose_name='Järjestävä taho')
-	email=models.EmailField(verbose_name='Sähköpostiosoite')
-	def __str__(self):
-		return "Tapahtuman järjestäjä(t): "+str(self.name)+", Sähköposti: "+str(self.email)
+    name = models.CharField(max_length=500, unique=True, verbose_name='Järjestävä taho')
+    email = models.EmailField(verbose_name='Sähköpostiosoite')
+
+    def __str__(self):
+        return "Tapahtuman järjestäjä(t): "+str(self.name)+", Sähköposti: "+str(self.email)
+
 
 # Kaikki tapahtumat koodusti.
 class Events(models.Model):
-	event_type=models.ForeignKey(EventType, to_field='event_type' ,on_delete=models.CASCADE)
-	uid=models.PositiveIntegerField(primary_key=True)
-#	owner=models.ForeignKey(EventOwner, to_field='name', on_delete=models.CASCADE)
-	owner=models.ForeignKey(User, to_field='username', on_delete=models.CASCADE)
-	def __str__(self):
-		return str(self.event_type)+", uid: "+str(self.uid)+", tapahtuman järjestäjä(t) : "+str(self.owner)
+    event_type = models.ForeignKey(EventType, to_field='event_type', on_delete=models.CASCADE)
+    uid = models.PositiveIntegerField(primary_key=True)
+#    owner=models.ForeignKey(EventOwner, to_field='name', on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, to_field='username', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.event_type)+", uid: "+str(self.uid)+", tapahtuman järjestäjä(t) : "+str(self.owner)
+
 
 # Eri tapahtumatyyppien yhteiset attribuutit.
 # Abstrakti yläluokka.
 class CommonInfo(models.Model):
-#	def user_directory_path(instance, filename):
-    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
-#		return 'events/user_{0}/{1}'.format(instance.user.id, filename)
+    # kaikki yhteiset attribuutit tähän
+    uid = models.ForeignKey(Events, on_delete=models.CASCADE, editable=False)
+    event_type = models.ForeignKey(EventType, to_field='event_type', on_delete=models.CASCADE, editable=False)
+#    owner=models.ForeignKey(EventOwner, to_field='name', on_delete=models.CASCADE,editable=False)
+    owner = models.ForeignKey(User, to_field='username', on_delete=models.CASCADE, editable=False)
+    name = models.CharField(max_length=500, verbose_name='Tapahtuman nimi')
+    place = models.CharField(max_length=200, verbose_name='Pitopaikka')
+    date = models.DateField(verbose_name='Tapahtuman pitopäivä')
+    start_time = models.TimeField(verbose_name='Tapahtuman alkamisaika', default='00:00:00')
+    description = models.TextField(verbose_name='Tapahtuman yleiskuvaus')
+    pic = models.ImageField(blank=True, null=True, verbose_name='Ilmoittautumislomakkeen kansikuva', upload_to='events/%Y/%m/')
+    prize = models.CharField(max_length=500, blank=True, null=True, verbose_name='Tapahtuman hinta')
+    max_participants = models.PositiveIntegerField(blank=True, null=True, verbose_name='Maksimimäärä osallistujia')
+    signup_starts = models.DateTimeField(verbose_name='Tapahtumaan ilmoittautuminen avautuu')
+    signup_ends = models.DateTimeField(blank=True, null=True, verbose_name='Tapahtumaan ilmoittautuminen sulkeutuu')
+    has_reserve_spots = models.BooleanField(verbose_name='Ota ilmoittautumisen varasijat käyttöön')
 
-	#kaikki yhteiset attribuutit tähän
-	uid=models.ForeignKey(Events, on_delete=models.CASCADE,editable=False)
-	event_type=models.ForeignKey(EventType, to_field='event_type', on_delete=models.CASCADE,editable=False)
-#	owner=models.ForeignKey(EventOwner, to_field='name', on_delete=models.CASCADE,editable=False)
-	owner=models.ForeignKey(User,to_field='username', on_delete=models.CASCADE,editable=False)
-	name=models.CharField(max_length=500, verbose_name='Tapahtuman nimi')
-	place=models.CharField(max_length=200, verbose_name='Pitopaikka')
-	date=models.DateField(verbose_name='Tapahtuman pitopäivä')
-	start_time=models.TimeField(verbose_name='Tapahtuman alkamisaika', default='00:00:00')
-	description=models.TextField(verbose_name='Tapahtuman yleiskuvaus')
-	pic=models.ImageField(blank=True, null=True,verbose_name='Ilmoittautumislomakkeen kansikuva', upload_to='events/%Y/%m/')
-	prize=models.CharField(max_length=500,blank=True, null=True,verbose_name='Tapahtuman hinta')
-	max_participants=models.PositiveIntegerField(blank=True, null=True,verbose_name='Maksimimäärä osallistujia')
-	signup_starts=models.DateTimeField(verbose_name='Tapahtumaan ilmoittautuminen avautuu')
-	signup_ends=models.DateTimeField(blank=True, null=True,verbose_name='Tapahtumaan ilmoittautuminen sulkeutuu')
-	has_reserve_spots=models.BooleanField(verbose_name='Ota ilmoittautumisen varasijat käyttöön')
+    def save(self, *args, **kwargs):
+        self.description = mark_safe(self.description.replace("\n", "<br/>"))
+        super(CommonInfo, self).save(*args, **kwargs)
 
-	def save(self, *args, **kwargs):
-		self.description = mark_safe(self.description.replace("\n", "<br/>"))
-		super(CommonInfo, self).save(*args, **kwargs)
+    def __str__(self):
+        return "Tapahtuman järjestäjä: "+str(self.owner)+", Tapahtuman tyyppi: "+str(self.event_type)+", Tapahtuman nimi: "+self.name+", Pitopaikka "+self.place+", Hinta: "+str(self.prize)+", Tapahtuman pitopäivä: "+str(self.date)+", Maksimi osallistujamäärä: "+str(self.max_participants)+", Ilmoittautuminen alkaa: "+str(self.signup_starts)+", Ilmoittautuminen loppuu: "+str(self.signup_ends)+", Yleiskuvaus: "+self.description
 
-	def __str__(self):
-		return "Tapahtuman järjestäjä: "+str(self.owner)+", Tapahtuman tyyppi: "+str(self.event_type)+", Tapahtuman nimi: "+self.name+", Pitopaikka "+self.place+", Hinta: "+str(self.prize)+", Tapahtuman pitopäivä: "+str(self.date)+", Maksimi osallistujamäärä: "+str(self.max_participants)+", Ilmoittautuminen alkaa: "+str(self.signup_starts)+", Ilmoittautuminen loppuu: "+str(self.signup_ends)+", Yleiskuvaus: "+self.description
+    def genInfo(self):
+        if not self.prize:
+            return "<li>Mikä: "+self.name+"</li><li>Missä: "+self.place+"</li><li>Milloin: "+str(self.date)+"</li><li>Mitä maksaa: Ilmainen</li>"
+        elif(self.prize==0):
+            return "<li>Mikä: "+self.name+"</li><li>Missä: "+self.place+"</li><li>Milloin: "+str(self.date)+"</li><li>Mitä maksaa: Ilmainen</li>"
+        else:
+            return "<p>Mikä-Missä-Milloin</p><p><ul><li>Mikä: "+self.name+"</li><li>Missä: "+self.place+"</li><li>Milloin: "+str(self.date)+"</li><li>Mitä maksaa: "+str(self.prize)+"</li>"
 
-	def genInfo(self):
-		if not self.prize:
-			return "<li>Mikä: "+self.name+"</li><li>Missä: "+self.place+"</li><li>Milloin: "+str(self.date)+"</li><li>Mitä maksaa: Ilmainen</li>"
-		elif(self.prize==0):
-			return "<li>Mikä: "+self.name+"</li><li>Missä: "+self.place+"</li><li>Milloin: "+str(self.date)+"</li><li>Mitä maksaa: Ilmainen</li>"
-		else:
-			return "<p>Mikä-Missä-Milloin</p><p><ul><li>Mikä: "+self.name+"</li><li>Missä: "+self.place+"</li><li>Milloin: "+str(self.date)+"</li><li>Mitä maksaa: "+str(self.prize)+"</li>"
+    class Meta:
+        abstract = True
 
-	class Meta:
-		abstract = True
 
 # Sitsit tyyppinen tapahtuma.
 class Sitz(CommonInfo):
-	quotas=models.CharField(max_length=500,null=True, blank=True,verbose_name='Järjestävien tahojen osallistujakiintiöt')
+    quotas = models.CharField(max_length=500, null=True, blank=True, verbose_name='Järjestävien tahojen osallistujakiintiöt')
 
-	def __str__(self):
-		if self.quotas is None:
-			return super().__str__()+", Osallistujakiintiöt: Ei ole"
-		else:
-			return super().__str__()+", Osallistujakiintiöt: "+self.quotas
+    def __str__(self):
+        if self.quotas is None:
+            return super().__str__()+", Osallistujakiintiöt: Ei ole"
+        else:
+            return super().__str__()+", Osallistujakiintiöt: "+self.quotas
+
 
 # Vuosijuhlat tyyppinen tapahtuman.
 class Annualfest(CommonInfo):
-	def __str__(self):
-		return super().__str__()
+    def __str__(self):
+        return super().__str__()
+
 
 # Ekskurisio tyyppinen tapahtuma.
 class Excursion(CommonInfo):
-	date=models.DateField(verbose_name='Ekskursion aloituspäivä')
-	end_date=models.DateField(verbose_name='Ekskursion loppumispäivä')
-	def __str__(self):
-		return super().__str__()+", Päättymispäivä: "+str(self.end_date)
+    date = models.DateField(verbose_name='Ekskursion aloituspäivä')
+    end_date = models.DateField(verbose_name='Ekskursion loppumispäivä')
+
+    def __str__(self):
+        return super().__str__()+", Päättymispäivä: "+str(self.end_date)
+
 
 # Muu ennalta määrittelemätön tapahtuma.
 class OtherEvent(CommonInfo):
-	min_participants=models.PositiveIntegerField(blank=True, null=True,verbose_name='Minimimäärä osallistujia')
-	def __str__(self):
-		return super().__str__()+", Minimimäärä osallistujia: "+str(self.min_participants)
+    min_participants = models.PositiveIntegerField(blank=True, null=True, verbose_name='Minimimäärä osallistujia')
+
+    def __str__(self):
+        return super().__str__()+", Minimimäärä osallistujia: "+str(self.min_participants)
+
 
 # Tapahtumaan osallistuja.
 class Participant(models.Model):
-	event_type=models.ForeignKey(Events, on_delete=models.CASCADE,editable=False)
-	name=models.CharField(max_length=200,verbose_name='Nimi')
-	email=models.EmailField(verbose_name='Sähköpostiosoite')
-	vege=models.NullBooleanField(blank=True,null=True)
-	nonholic=models.NullBooleanField(blank=True,null=True)
-	avec=models.CharField(max_length=100,blank=True,null=True)
-	plaseeraus=models.CharField(max_length=500,blank=True,null=True)
-	quota=models.CharField(max_length=200,blank=True,null=True)
-	reserve_spot=models.BooleanField()
-#	Tämä kenttä sisältää tiedot: jäsen/ei jäsen, onko maksanut.
-#	datan tulee olla muodossa {member:arvo, hasPaid:arvo}
-	miscInfo=models.TextField(editable=False)
-	def __str__(self):
-		return str(self.event_type)+', '+ self.name+" ("+self.email+"), vege: "+str(self.vege)+", holiton: "+str(self.nonholic)+', avec: '+str(self.avec)+', plaseeraus: '+str(self.plaseeraus)+', quota: '+str(self.quota)+', muut tiedot:'+self.miscInfo
+    event_type = models.ForeignKey(Events, on_delete=models.CASCADE, editable=False)
+    name = models.CharField(max_length=200, verbose_name='Nimi')
+    email = models.EmailField(verbose_name='Sähköpostiosoite')
+    vege = models.NullBooleanField(blank=True, null=True)
+    nonholic = models.NullBooleanField(blank=True, null=True)
+    avec = models.CharField(max_length=100, blank=True, null=True)
+    plaseeraus = models.CharField(max_length=500, blank=True, null=True)
+    quota = models.CharField(max_length=200, blank=True, null=True)
+    reserve_spot = models.BooleanField()
+#    Tämä kenttä sisältää tiedot: jäsen/ei jäsen, onko maksanut.
+#    datan tulee olla muodossa {member:arvo, hasPaid:arvo}
+    miscInfo = models.TextField(editable=False)
+
+    def __str__(self):
+        return str(self.event_type)+', '+ self.name+" ("+self.email+"), vege: "+str(self.vege)+", holiton: "+str(self.nonholic)+', avec: '+str(self.avec)+', plaseeraus: '+str(self.plaseeraus)+', quota: '+str(self.quota)+', muut tiedot:'+self.miscInfo
+
 
 # Arkistotaulu.
 class Archive(models.Model):
-	event_type=models.CharField(max_length=500,verbose_name='Tapahtuman typpi')
-	name=models.CharField(max_length=500,verbose_name='Tapahtuman nimi')
-	description=models.TextField(verbose_name='Tapahtuman yleiskuvaus')
-	participants=models.IntegerField(verbose_name='Osallistujamäärä')
-	owner=models.CharField(max_length=500,verbose_name='Tapahtuman pitäjä')
-	date=models.DateTimeField(verbose_name='Tapahtuman pitopäivä')
-	place=models.CharField(max_length=200, verbose_name='Pitopaikka')
-	pic=models.ImageField(blank=True, null=True,verbose_name='Ilmoittautumislomakkeen kansikuva')
-	prize=models.CharField(max_length=500,blank=True, null=True,verbose_name='Tapahtuman hinta')
-	def __str__(self):
-		return "Tapahtuman tyyppi: "+self.event_type+", Tapahtuman nimi: "+self.name+", Kokonaisosallistujamäärä: "+str(self.participants)+", Tapahtuman järjestäjä: "+self.owner+",Alkuperäinen pitopäivä: "+str(self.date)+", Yleiskuvaus: "+self.description+', Alkuperäinen pitopaikka: '+self.place+', Mitä maksoi: '+self.prize
+    event_type = models.CharField(max_length=500, verbose_name='Tapahtuman typpi')
+    name = models.CharField(max_length=500, verbose_name='Tapahtuman nimi')
+    description = models.TextField(verbose_name='Tapahtuman yleiskuvaus')
+    participants = models.IntegerField(verbose_name='Osallistujamäärä')
+    owner = models.CharField(max_length=500, verbose_name='Tapahtuman pitäjä')
+    date = models.DateTimeField(verbose_name='Tapahtuman pitopäivä')
+    place = models.CharField(max_length=200, verbose_name='Pitopaikka')
+    pic = models.ImageField(blank=True, null=True, verbose_name='Ilmoittautumislomakkeen kansikuva')
+    prize = models.CharField(max_length=500, blank=True, null=True, verbose_name='Tapahtuman hinta')
 
+    def __str__(self):
+        return "Tapahtuman tyyppi: "+self.event_type+", Tapahtuman nimi: "+self.name+", Kokonaisosallistujamäärä: "+str(self.participants)+", Tapahtuman järjestäjä: "+self.owner+",Alkuperäinen pitopäivä: "+str(self.date)+", Yleiskuvaus: "+self.description+', Alkuperäinen pitopaikka: '+self.place+', Mitä maksoi: '+self.prize
--- omat/helpers.py
+++ omat/helpers.py
@@ -3,145 +3,156 @@
 from eventsignup.models import Events, Sitz, Annualfest, Excursion, OtherEvent, Participant
 from eventsignup.forms import AnnualfestForm, ExcursionForm, OtherEventForm, SitzForm
 from eventsignup.forms import SitzSignupForm, AnnualfestSignupForm, ExcursionSignupForm, OtherEventSignupForm, CustomSignupForm
-import random, json
+import random
+import json
 from django.core import mail
+
 
 # Generoi uuden uniikin uid:n tapahtumalle.
 def getUid():
-	uid=random.randint(10000, 99999)
-	events=Events.objects.all().values('uid')
-	for field in events:
-		if(field['uid']==uid):
-			uid=random.randint(10000, 99999)
-	return uid
+    uid = random.randint(10000, 99999)
+    events = Events.objects.all().values('uid')
+    for field in events:
+        if(field['uid'] == uid):
+            uid = random.randint(10000, 99999)
+    return uid
+
 
 # Palauttaa oikean tyyppisen form-olion, jotta saadaan oikeanlainen
 # tapahtuma tallennettua.
-def getForm(event_type,request):
-	form=None
-	if(event_type=='sitsit'):
-		form=SitzForm(request.POST,request.FILES)
-	elif(event_type=='vuosijuhlat'):
-		form=AnnualfestForm(request.POST,request.FILES)
-	elif(event_type=='ekskursio'):
-		form=ExcursionForm(request.POST,request.FILES)
-	elif(event_type=='muutapahtuma'):
-		form=OtherEventForm(request.POST,request.FILES)
-	return form
+def getForm(event_type, request):
+    form = None
+    if(event_type == 'sitsit'):
+        form = SitzForm(request.POST, request.FILES)
+    elif(event_type == 'vuosijuhlat'):
+        form = AnnualfestForm(request.POST, request.FILES)
+    elif(event_type == 'ekskursio'):
+        form = ExcursionForm(request.POST, request.FILES)
+    elif(event_type == 'muutapahtuma'):
+        form = OtherEventForm(request.POST, request.FILES)
+    return form
+
 
 # Palauttaa oikeanlaisen form-olion, jotta saadaan
 # oikeanlainen tapahtumaanilmoittautuminen.
-def getSignupForm(event_type,request):
-	form=None
-	if(event_type=='sitsit'):
-		form=SitzSignupForm(request.POST)
-	elif(event_type=='vuosijuhlat'):
-		form=AnnualfestSignupForm(request.POST)
-	elif(event_type=='ekskursio'):
-		form=ExcursionSignupForm(request.POST)
-	elif(event_type=='muutapahtuma'):
-		form=OtherEventSignupForm(request.POST)
-	elif(event_type=='custom'):
-		form=CustomSignupForm(request.POST)
-	return form
+def getSignupForm(event_type, request):
+    form = None
+    if(event_type == 'sitsit'):
+        form = SitzSignupForm(request.POST)
+    elif(event_type == 'vuosijuhlat'):
+        form = AnnualfestSignupForm(request.POST)
+    elif(event_type == 'ekskursio'):
+        form = ExcursionSignupForm(request.POST)
+    elif(event_type == 'muutapahtuma'):
+        form = OtherEventSignupForm(request.POST)
+    elif(event_type == 'custom'):
+        form = CustomSignupForm(request.POST)
+    return form
+
 
 # Palauttaa tietokannasta oikeanlaisen tapahtuman
 # esikatselua varten.
 def getEvent(uid):
-	event=None
-	tempevent=Events.objects.get(uid=uid)
-	if(tempevent.event_type.event_type=='sitsit'):
-		event=Sitz.objects.get(uid=uid)
-	elif(tempevent.event_type.event_type=='vuosijuhlat'):
-		event=Annualfest.objects.get(uid=uid)
-	elif(tempevent.event_type.event_type=='ekskursio'):
-		event=Excursion.objects.get(uid=uid)
-	elif(tempevent.event_type.event_type=='muutapahtuma'):
-		event=OtherEvent.objects.get(uid=uid)
-	return event
+    event = None
+    tempevent = Events.objects.get(uid=uid)
+    if(tempevent.event_type.event_type == 'sitsit'):
+        event = Sitz.objects.get(uid=uid)
+    elif(tempevent.event_type.event_type == 'vuosijuhlat'):
+        event = Annualfest.objects.get(uid=uid)
+    elif(tempevent.event_type.event_type == 'ekskursio'):
+        event = Excursion.objects.get(uid=uid)
+    elif(tempevent.event_type.event_type == 'muutapahtuma'):
+        event = OtherEvent.objects.get(uid=uid)
+    return event
+
 
 # Generoi sähköpostin viestiosan.
-def genMsg(data,request):
-	prize=""
-	try:
-		if "€" in data.prize:
-			prize=str(data.prize)
-		else:
-			prize=str(data.prize)+" €"
-	except:
-		prize="ilmainen"
-	return data.name+"\n\n"+str(data.owner)+"\n\n"+data.description+"\n\nIlmoittaudu tästä: "+getBaseurl(request)+"/event/"+str(data.uid.uid)+"/signup\n\nTL;DR:\n\nMikä: "+data.name+"\nMissä: "+data.place+"\nMilloin: "+str(data.date)+" klo: "+str(data.start_time)+"\nMitä maksaa: "+prize+"\n"
+def genMsg(data, request):
+    prize = ""
+    try:
+        if "€" in data.prize:
+            prize = str(data.prize)
+        else:
+            prize = str(data.prize)+" €"
+    except:
+        prize = "ilmainen"
+    return data.name+"\n\n"+str(data.owner)+"\n\n"+data.description+"\n\nIlmoittaudu tästä: "+getBaseurl(request)+"/event/"+str(data.uid.uid)+"/signup\n\nTL;DR:\n\nMikä: "+data.name+"\nMissä: "+data.place+"\nMilloin: "+str(data.date)+" klo: "+str(data.start_time)+"\nMitä maksaa: "+prize+"\n"
+
 
 # Lähettää tapahtuman tiedot
 # käyttäjälle rekisteröityyn sähköpostiosoitteeseen.
-def sendEmail(data,request):
-	with mail.get_connection() as connection:
-	    mail.EmailMessage(
+def sendEmail(data, request):
+    with mail.get_connection() as connection:
+        mail.EmailMessage(
        ' [* tapahtumailmoittautumisjärjestelmä] Lisätty tapahtuma: '+data.name, genMsg(data,request), 'noreply@asteriski.fi', [str(data.owner.email)],
         connection=connection,
-   		 ).send()
-#	send_mail(
+            ).send()
+#    send_mail(
 #   '[* tapahtumailmoittautumisjärjestelmä] Lisätty tapahtuma: '+data.name,
 #    genMsg(data,request),
 #    'noreply@asteriski.fi',
 #    [request.user.email],
 #    fail_silently=False,
-#	)
+#    )
+
 
 # Palauttaa listan, jossa on järjestävien tahojen nimet, mikäli on osallistujakiintiöitä.
-def getQuotaNames(quotas,namesOnly):
-	temp=quotas.split(",")
-	paluu=[]
-	temp2=[]
-	if(':' in temp[0]):
-		for x in temp:
-			temp2.append(x.split(":"))
-	else:
-		temp3=[]
-		for x in temp:
-			temp3.append(x.split())
-		for x in temp3:
-			temp4=''
-			for y in x:
-				if not str.isdigit(y):
-					temp4+=y
-				temp2.append([temp4,temp3[-1]])
+def getQuotaNames(quotas, namesOnly):
+    temp = quotas.split(",")
+    paluu = []
+    temp2 = []
+    if(':' in temp[0]):
+        for x in temp:
+            temp2.append(x.split(":"))
+    else:
+        temp3 = []
+        for x in temp:
+            temp3.append(x.split())
+        for x in temp3:
+            temp4 = ''
+            for y in x:
+                if not str.isdigit(y):
+                    temp4 += y
+                temp2.append([temp4, temp3[-1]])
 
-	if(namesOnly):
-		for y in temp2:
-			paluu.append(y[0])
-	else:
-		return temp2
-	return paluu
+    if(namesOnly):
+        for y in temp2:
+            paluu.append(y[0])
+    else:
+        return temp2
+    return paluu
+
 
 # Generoi tapahtumaan ilmoittautujaan tallennettavia lisätietoja.
 def getMiscInfo(data):
-	return json.dumps({'member':False, 'hasPaid':False})
+    return json.dumps({'member': False, 'hasPaid': False})
+
 
 def getBaseurl(request):
-	protocol='http'
-	if(request.is_secure()):
-		protocol='https'
-	return protocol+'://'+request.META['HTTP_HOST']
+    protocol = 'http'
+    if(request.is_secure()):
+        protocol = 'https'
+    return protocol+'://'+request.META['HTTP_HOST']
+
 
 # Palauttaa dictionaryn, jossa on {uid:osallistujamäärä}
 def getParticipantCount():
-	events=Events.objects.all()
-	numOfParticipants={}
-	for event in events:
-		uid=str(event.uid)
-		numOfParticipants[uid]=Participant.objects.filter(event_type_id=event.uid).count()
-		#numOfParticipants.update(uid=Participant.objects.filter(event_type_id=event.uid).count())
-	return numOfParticipants
+    events = Events.objects.all()
+    numOfParticipants = {}
+    for event in events:
+        uid = str(event.uid)
+        numOfParticipants[uid] = Participant.objects.filter(event_type_id=event.uid).count()
+        # numOfParticipants.update(uid=Participant.objects.filter(event_type_id=event.uid).count())
+    return numOfParticipants
+
 
 # Palauttaa True/False riippuen siitä onko ilmoittautumiskiintiö täynnä.
-
-def isQuotaFull(event,data):
-	try:
-		#	haettu quota on data['name']
-		quotas=getQuotaNames(event.quotas,False)
-		numOf=Participant.objects.filter(event_type=event.uid).filter(miscInfo__contains=data['name']).count()
-		paluu=False
-	except AttributeError as e:
-		paluu=False
-	return paluu
+def isQuotaFull(event, data):
+    try:
+        #    haettu quota on data['name']
+        quotas = getQuotaNames(event.quotas, False)
+        numOf = Participant.objects.filter(event_type=event.uid).filter(miscInfo__contains=data['name']).count()
+        paluu = False
+    except AttributeError as e:
+        paluu = False
+    return paluu
