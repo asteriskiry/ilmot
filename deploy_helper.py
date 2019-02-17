6c4ad5450eed7038353165312f107778c8f13a78
aegan123 <juhani@fmail.co.uk>
2019-02-17 22:48:40 +0200

    updated deployment script

--- deploy_helper.py
+++ deploy_helper.py
@@ -4,20 +4,24 @@ import sys
 import urllib.request
 import shutil
 import zipfile
+import subprocess
 
 
-# Luo tietokantaan muut vaaditut datat sekä
+# Valmistelee tietokannan Djangon omalla migraatio työkalulla ja
+# lisää tietokantaan muut vaaditut datat sekä
 # luo superuserin sekä käyttäjän, jolla on oikeat oikeudet kantaan.
 def setupDjango():
-    global eventTypes, accounts
+    print('Laitetaan Django käyttökuntoon.\n')
+    subprocess.run(["python3", "manage.py", "makemigrations"])
+    subprocess.run(["python3", "manage.py", "migrate"])
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "riskiwww.settings")
     import django
     django.setup()
+    print('Lisätään kaikki EventTypes tietokantaan.\n')
     from eventsignup.models import EventType
     for x in ['custom', 'muutapahtuma', 'ekskursio', 'vuosijuhlat', 'sitz', 'sitsit']:
-        etype=EventType(event_type=x)
+        etype = EventType(event_type=x)
         etype.save()
-    eventTypes = True
     from django.contrib.auth.models import User
 #    from eventsignup.models import EventOwner
     print('Luodaan uusi superuser.')
@@ -44,7 +48,7 @@ def setupDjango():
 # Lataa ja purkaa Bulma css:n tiedostot oikeaan paikkaan.
 def setupBulma():
     global bulma
-    print('Asennetaan Bulma css.')
+    print('Ladataan ja puretaan Bulma css.\n')
     with urllib.request.urlopen('https://github.com/jgthms/bulma/releases/download/0.7.2/bulma-0.7.2.zip') as response, open('delete_me.zip', 'wb') as out_file:
         shutil.copyfileobj(response, out_file)
     with zipfile.ZipFile('./delete_me.zip', 'r') as zipref:
@@ -53,38 +57,137 @@ def setupBulma():
     shutil.rmtree('./mybulma/__MACOSX/')
 
 
-def setupEnv():
+def getDBInfo():
+    db_name = input('Anna tietokannan nimi: ')
+    db_user = input('Anna tietokantaan tarvittava käyttäjätunnus: ')
+    db_pw = input('Anna salasana: ')
+    db_host = input('Anna tietokantapalvelimen ip osoite: ')
+    db_port = input('Anna käytetty portti: ')
+    return [db_name, db_user, db_pw, db_host, db_port]
+
+
+def getSmtpInfo():
+    email_host = input('Anna smtp palvelimen nimi: ')
+    email_user = input('Anna smtp palvelimen käyttäjänimi (jos tarvitaan): ')
+    email_pw = input('Anna salasana (jos tarvitaan): ')
+    email_port = input('Anna smtp palvelimen käyttämä portti: ')
+    return [email_host, email_user, email_pw, email_port]
+
+
+# Valmistelee järjestelmätason käyttöympäristön kuntoon ja
+# generoi Djangolle tarvittavat konffit.
+def setupEnv(rerun):
+    if(not rerun):
+        print('Asennetaan tarvittavat python moduulit.\n')
+        try:
+            with open('./requirements.txt', 'r') as f:
+                modules = []
+                for line in f:
+                    modules.append(line.replace('\n', ''))
+                for module in modules:
+                    subprocess.run(["pip3", "install",  module])
+        except IOError:
+            print('Tiedosto requirements.txt ei löydy! Skriptin suoritusta ei voi jatkaa. Varmista, että repo on kloonattu kunnolla.\n')
+            exit(1)
     from django.core.management.utils import get_random_secret_key
-    print('Luodaa tarvittava .env tiedosto.')
+    print('Luodaan tarvittava .env tiedosto.\n')
+    forcedInfo = False
+    if(rerun):
+        oldDBSettings = ''
+        oldSmtpSettings = []
+        oldUrl = ''
+        try:
+            with open('./riskiwww/.env', 'r') as f:
+                for line in f:
+                    if('DATABASE_URL' in line):
+                        oldDBSettings = line
+                    if('EMAIL' in line):
+                        oldSmtpSettings.append(line)
+                    if('ALLOWED_HOSTS' in line):
+                        oldUrl = line
+        except IOError:
+            print('Virhe! Vanhaa .env tiedostoa ei löydy. Asetusten uudelleen määrittely on pakollista.\n')
+            forcedInfo = True
     with open('./riskiwww/.env', 'w') as f:
-        f.write('SECRET_KEY='+get_random_secret_key())
-        f.write('DEBUG=False')
-        f.write('ALLOWED_HOSTS=.localhost,127.0.0.1,ilmot.asteriski.fi')
-        db_name = input('Anna tietokannan nimi: ')
-        db_user = input('Anna tietokantaan tarvittava käyttäjätunnus: ')
-        db_pw = input('Anna salasana: ')
-        db_host = input('Anna tietokantapalvelimen ip osoite: ')
-        db_port = input('Anna käytetty portti: ')
-        f.write("DATABASE_URL={'ENGINE': 'django.db.backends.mysql','NAME': '"+db_name+"','USER': '"+db_user+"','PASSWORD': '"+db_pw+"',    'HOST': '"+db_host+"','PORT': '"+db_port+"',}}")
-        f.write('EMAIL_BACKEND=django.core.mail.backends.smtp.EmailBackend')
-        email_host = input('Anna smtp palvelimen nimi: ')
-        f.write('EMAIL_HOST='+email_host)
-        email_user = input('Anna smtp palvelimen käyttäjänimi (jos tarvitaan): ')
-        email_pw = input('Anna salasana (jos tarvitaan): ')
-        if(not (email_pw is None)):
-            f.write('EMAIL_HOST_PASSWORD='+email_pw)
-        if(not(email_user is None)):
-            f.write('EMAIL_HOST_USER='+email_user)
-        f.write('EMAIL_PORT=')
-        f.write('SECURE_CONTENT_TYPE_NOSNIFF=True')
-        f.write('SECURE_BROWSER_XSS_FILTER=True')
-        f.write('SESSION_COOKIE_SECURE=True')
-        f.write('CSRF_COOKIE_SECURE=True')
-        f.write('X_FRAME_OPTIONS=DENY')
-        f.write('SESSION_COOKIE_SECURE=True')
+        writeDB = False
+        writeSmtp = False
+        writeHost = False
+        url = ''
+        f.write('SECRET_KEY='+get_random_secret_key()+'\n')
+        f.write('DEBUG=False\n')
+        if(forcedInfo or (rerun and input('Uusitaanko url tiedot? [k/e]: ').casefold() == 'k')):
+            url = input('Anna url osoite, josta palvelun saa kiinni (eg. url.asteriski.fi) (lisätään allowed host tietoihin): ')
+            writeHost = True
+        elif(not rerun):
+            url = input('Anna url osoite, josta palvelun saa kiinni (eg. url.asteriski.fi) (lisätään allowed host tietoihin): ')
+            writeHost = True
+        if(writeHost):
+            f.write('ALLOWED_HOSTS=.localhost,127.0.0.1,'+url+'\n')
+        if(forcedInfo or (rerun and input('Uusitaanko tietokannan tiedot? [k/e]: ').casefold() == 'k')):
+            dbInfo = getDBInfo()
+            writeDB = True
+        elif(not rerun):
+            dbInfo = getDBInfo()
+            writeDB = True
+        if(writeDB):
+            f.write("DATABASE_URL={'ENGINE': 'django.db.backends.mysql','NAME': '"+dbInfo[0]+"','USER': '"+dbInfo[1]+"','PASSWORD': '"+dbInfo[2]+"','HOST': '"+dbInfo[3]+"','PORT': '"+dbInfo[4]+"',}}\n")
+        f.write('EMAIL_BACKEND=django.core.mail.backends.smtp.EmailBackend\n')
+        if(forcedInfo or (rerun and input('Uusitaanko smtp asetukset? [k/e]: ').casefold() == 'k')):
+            smtpInfo = getSmtpInfo()
+            writeSmtp = True
+        elif(not rerun):
+            smtpInfo = getSmtpInfo()
+            writeSmtp = True
+        if(writeSmtp):
+            f.write('EMAIL_HOST='+smtpInfo[0]+'\n')
+            if(not (smtpInfo[1] is None)):
+                f.write('EMAIL_HOST_PASSWORD='+smtpInfo[1]+'\n')
+            if(not(smtpInfo[2] is None)):
+                f.write('EMAIL_HOST_USER='+smtpInfo[2]+'\n')
+            f.write('EMAIL_PORT='+smtpInfo[3]+'\n')
+        if(rerun and not writeDB):
+            f.write(oldDBSettings)
+            f.write('\n')
+        if(rerun and not writeSmtp):
+            for line in oldSmtpSettings:
+                f.write(line+'\n')
+            f.write('\n')
+        if(rerun and not writeHost):
+            f.write(oldUrl+'\n')
+        f.write('SECURE_CONTENT_TYPE_NOSNIFF=True\n')
+        f.write('SECURE_BROWSER_XSS_FILTER=True\n')
+        f.write('SESSION_COOKIE_SECURE=True\n')
+        f.write('CSRF_COOKIE_SECURE=True\n')
+        f.write('X_FRAME_OPTIONS=DENY\n')
+        f.write('SESSION_COOKIE_SECURE=True\n\n')
+
+
+# Kääntää css:n valmiiksi käytettävään muotoon.
+def setupCss():
+    print('Käännetään css.')
+    import sass
+    path = os.getcwd()
+    sass.compile(dirname=(path+"/mybulma/sass/", path+"/static/css/"))
+
+
+def printEnv():
+    with open('./riskiwww/.env', 'r') as f:
+        print('.env tiedosto luotiin seuraavilla tiedoilla. Mikäli syötetyissä tiedoissa oli virheitä, aja skripti uudestaan: python3 '+sys.argv[0]+' -r')
+        for line in f:
+            print(line)
 
 
 if __name__ == '__main__':
-    setupEnv()
-    setupDjango()
-    setupBulma()
+    if(len(sys.argv) > 1):
+        if(sys.argv[1] == '-r'):
+            setupEnv(True)
+            printEnv()
+            exit()
+        else:
+            exit()
+    else:
+        setupEnv(False)
+        setupDjango()
+        setupBulma()
+        setupCss()
+        printEnv()
