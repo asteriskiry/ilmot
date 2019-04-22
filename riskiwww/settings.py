b67f304e640827b41ad5e2f1751e2b96cb6b0eab
aegan123 <juhani@fmail.co.uk>
2019-04-22 15:13:35 +0300

    Manually syncked settings file

--- riskiwww/settings.py
+++ riskiwww/settings.py
@@ -6,14 +6,20 @@ Django settings for riskiwww project.
 import os
 import dj_database_url
 from decouple import config, Csv
+from django.core.management.utils import get_random_secret_key
+
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-SECRET_KEY = config('SECRET_KEY')
+if DEBUG:
+    SECRET_KEY = get_random_secret_key()
+else:
+    SECRET_KEY = config('SECRET_KEY')
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+if not DEBUG:
+    ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 # Application definition
 
@@ -60,20 +66,24 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'riskiwww.wsgi.application'
 
-#DATABASES = {
-#    'default': dj_database_url.config(config('DATABASE_URL')
-#    )
-#}
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'OPTIONS': {
-            'read_default_file': './my.cnf',
-            'init_command': 'SET storage_engine=INNODB',
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-        },
+if DEBUG:
+    DATABASES = {
+        'default': dj_database_url.config(
+        default='sqlite:///./db.sqlite3'
+        )
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'OPTIONS': {
+                'read_default_file': './my.cnf',
+                'init_command': 'SET storage_engine=INNODB',
+                'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+            },
+        }
+    }
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -92,6 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 # Internationalization
+# https://docs.djangoproject.com/en/2.0/topics/i18n/
 
 LANGUAGE_CODE = 'fi-fi'
 
@@ -107,25 +118,33 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 
 STATIC_URL = '/static/'
-#STATICFILES_DIRS = [
-#    os.path.join(BASE_DIR, 'static'),
-#]
-STATIC_ROOT=config('STATIC_ROOT')
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static'),
+    ]
+else:
+    STATIC_ROOT=config('STATIC_ROOT')
 # Muut conffit
 LOGIN_REDIRECT_URL = 'management'
 LOGOUT_REDIRECT_URL = '/'
-EMAIL_BACKEND = config('EMAIL_BACKEND')
+if DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+else:
+    EMAIL_BACKEND = config('EMAIL_BACKEND')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 FIRST_DAY_OF_WEEK = 1
+if not DEBUG:
+    EMAIL_HOST = config('EMAIL_HOST')
+    EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+    EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+    EMAIL_PORT = config('EMAIL_PORT')
+    SECURE_CONTENT_TYPE_NOSNIFF = config('SECURE_CONTENT_TYPE_NOSNIFF', cast=bool)
+    SECURE_BROWSER_XSS_FILTER = config('SECURE_BROWSER_XSS_FILTER', cast=bool)
+    SESSION_COOKIE_SECURE = config('SESSION_COOKIE_SECURE', cast=bool)
+    CSRF_COOKIE_SECURE = config('CSRF_COOKIE_SECURE', cast=bool)
+    X_FRAME_OPTIONS = config('X_FRAME_OPTIONS')
+    SESSION_COOKIE_SECURE = config('SESSION_COOKIE_SECURE', cast=bool)
 
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_PORT = config('EMAIL_PORT')
-SECURE_CONTENT_TYPE_NOSNIFF = config('SECURE_CONTENT_TYPE_NOSNIFF', cast=bool)
-SECURE_BROWSER_XSS_FILTER = config('SECURE_BROWSER_XSS_FILTER', cast=bool)
-SESSION_COOKIE_SECURE = config('SESSION_COOKIE_SECURE', cast=bool)
-CSRF_COOKIE_SECURE = config('CSRF_COOKIE_SECURE', cast=bool)
-X_FRAME_OPTIONS = config('X_FRAME_OPTIONS')
-SESSION_COOKIE_SECURE = config('SESSION_COOKIE_SECURE', cast=bool)
+
+
