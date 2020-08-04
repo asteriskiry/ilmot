194c6360c33d9f7da831b1bb4c57fa79dab98166
aegan123 <juhani@fmail.co.uk>
2020-08-04 17:58:34 +0300

    Updated settings.py to have django-fobi in use and properly configured.

--- riskiwww/settings.py
+++ riskiwww/settings.py
@@ -27,12 +27,68 @@ INSTALLED_APPS = [
     'accounts.apps.AccountsConfig',
     'eventsignup.apps.EventsignupConfig',
     'widget_tweaks',
+    # django-admin-tools
+    'admin_tools',
+    'admin_tools.theming',
+    'admin_tools.menu',
+    'admin_tools.dashboard',
+    # / django-admin-tools
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Needed by fobi
+    'django.contrib.sites',
+    # Fobi stuff
+    # Fobi core
+    'fobi',
+    # `django-fobi` themes
+    'fobi.contrib.themes.bootstrap3',  # Bootstrap 3 theme
+    'fobi.contrib.themes.foundation5',  # Foundation 5 theme
+    'fobi.contrib.themes.simple',  # Simple theme
+    # Form field plugins
+    'fobi.contrib.plugins.form_elements.fields.boolean',
+    'fobi.contrib.plugins.form_elements.fields.checkbox_select_multiple',
+    'fobi.contrib.plugins.form_elements.fields.date',
+    'fobi.contrib.plugins.form_elements.fields.date_drop_down',
+    'fobi.contrib.plugins.form_elements.fields.datetime',
+    'fobi.contrib.plugins.form_elements.fields.decimal',
+    'fobi.contrib.plugins.form_elements.fields.email',
+    'fobi.contrib.plugins.form_elements.fields.file',
+    'fobi.contrib.plugins.form_elements.fields.float',
+    'fobi.contrib.plugins.form_elements.fields.hidden',
+    'fobi.contrib.plugins.form_elements.fields.input',
+    'fobi.contrib.plugins.form_elements.fields.integer',
+    'fobi.contrib.plugins.form_elements.fields.ip_address',
+    'fobi.contrib.plugins.form_elements.fields.null_boolean',
+    'fobi.contrib.plugins.form_elements.fields.password',
+    'fobi.contrib.plugins.form_elements.fields.radio',
+    'fobi.contrib.plugins.form_elements.fields.regex',
+    'fobi.contrib.plugins.form_elements.fields.select',
+    'fobi.contrib.plugins.form_elements.fields.select_model_object',
+    'fobi.contrib.plugins.form_elements.fields.select_multiple',
+    'fobi.contrib.plugins.form_elements.fields.select_multiple_model_objects',
+    'fobi.contrib.plugins.form_elements.fields.slug',
+    'fobi.contrib.plugins.form_elements.fields.text',
+    'fobi.contrib.plugins.form_elements.fields.textarea',
+    'fobi.contrib.plugins.form_elements.fields.time',
+    'fobi.contrib.plugins.form_elements.fields.url',
+
+    # Form element plugins
+    'easy_thumbnails',  # Required by ``content_image`` plugin
+    'fobi.contrib.plugins.form_elements.content.content_image',
+    'fobi.contrib.plugins.form_elements.content.content_image_url',
+    'fobi.contrib.plugins.form_elements.content.content_text',
+    'fobi.contrib.plugins.form_elements.content.content_video',
+
+    # Form handlers
+    'fobi.contrib.plugins.form_handlers.db_store',
+    'fobi.contrib.plugins.form_handlers.http_repost',
+    'fobi.contrib.plugins.form_handlers.mail',
+    'fobi.contrib.plugins.form_handlers.mail_sender',
+    # / Fobi stuff
 ]
 
 MIDDLEWARE = [
@@ -50,8 +106,9 @@ ROOT_URLCONF = 'riskiwww.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
+        'DIRS': [os.path.join(BASE_DIR, 'eventsignup', 'templates'),
+                 os.path.join(BASE_DIR, 'accounts', 'templates')],
+        # 'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.media',
@@ -59,6 +116,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                "fobi.context_processors.theme",  # Important!
+                "fobi.context_processors.dynamic_values",  # Optional
+            ],
+            'loaders': [
+                'django.template.loaders.filesystem.Loader',
+                'django.template.loaders.app_directories.Loader',
+                'admin_tools.template_loaders.Loader',
             ],
         },
     },
@@ -123,8 +187,15 @@ if DEBUG:
         os.path.join(BASE_DIR, 'static'),
     ]
 else:
-    STATIC_ROOT=config('STATIC_ROOT')
-# Muut conffit
+    STATIC_ROOT = config('STATIC_ROOT')
+
+# Fobi configs
+FOBI_DEFAULT_THEME = 'foundation5'
+# TODO check this for production!! (and other dev)
+SITE_ID = config('SITE_ID', default=2)
+# FOBI_RESTRICT_PLUGIN_ACCESS = False
+
+# other configs
 LOGIN_REDIRECT_URL = 'management'
 LOGOUT_REDIRECT_URL = '/'
 if DEBUG:
@@ -144,7 +215,5 @@ if not DEBUG:
     SESSION_COOKIE_SECURE = config('SESSION_COOKIE_SECURE', cast=bool)
     CSRF_COOKIE_SECURE = config('CSRF_COOKIE_SECURE', cast=bool)
     X_FRAME_OPTIONS = config('X_FRAME_OPTIONS')
-    SESSION_COOKIE_SECURE = config('SESSION_COOKIE_SECURE', cast=bool)
-
 
 
